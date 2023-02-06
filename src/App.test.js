18fba51b0acfbdf222ed497f6460a567e9cad7e4
Júlio Cezar Mendo <mendo.julio.c@gmail.com>
2023-02-06 10:28:45 -0300

    test and code for gray background when button disabled

--- src/App.test.js
+++ src/App.test.js
@@ -44,8 +44,32 @@ test('Checkbox disables button on first click and enables button on second click
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
   expect(colorButton).not.toBeEnabled();
 
+  // check that button color is gray
+  expect(colorButton).toHaveStyle({ 'background-color': 'gray' });
+
   // check that button is enabled on second click
   fireEvent.click(checkbox);
   expect(checkbox).not.toBeChecked();
   expect(colorButton).toBeEnabled();
+
+  // check that button color is back to red
+  expect(colorButton).toHaveStyle({ 'background-color': 'red' });
+});
+
+test('Checkbox disables button when it is blue, changes to gray and reverts back to blue', () => {
+  render(<App />);
+  // get button and click it
+  const button = screen.getByRole('button', { name: 'Change to blue' });
+  fireEvent.click(button);
+
+  // get checkbox and click it
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
+  fireEvent.click(checkbox);
+
+  // check that button color went to gray
+  expect(button).toHaveStyle({ 'background-color': 'gray' });
+
+  // click checkbox again and expect blue button
+  fireEvent.click(checkbox);
+  expect(button).toHaveStyle({ 'background-color': 'blue' });
 });
